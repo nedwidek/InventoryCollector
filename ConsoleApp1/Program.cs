a11517bf635568d2e37768481e7448c60152102d
Erik Nedwidek <nedwidek@obx.dev>
2021-01-14 13:34:15 -0500

    Added improvements
    - Collects IPv4 address
    - Collects public address
    - Can specify asset tag on CLI
    
    Client will try every 5 minutes to post data until successful.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Management;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Threading;
+
 namespace dev.obx.InventoryCollector
 {
+    /// <summary>
+    /// Driver for SystemInfo. Process args, collect system info, post data. Collected data is dumped to console for debug (TODO: maybe make option).
+    /// Arguments are URL of dataloader script (required) and asset tag number (optional).
+    /// </summary>
+    /// <example>
+    /// InventoryCollector.exe https://host.of.inventory.system/path/to/dataloader.php 123456
+    /// </example>
     class Program
     {
         static void Main(string[] args)
         {
-
-            if(args.Length != 1)
+            // Error out if we have too few or too many args
+            if(args.Length > 2 || args.Length == 0)
             {
                 Console.WriteLine("Please specify URL.");
                 Environment.Exit(1);
@@ -19,6 +28,7 @@ namespace dev.obx.InventoryCollector
 
             string url = args[0];
 
+            // Error out if the URL is not absolute and well formed.
             if(!Uri.IsWellFormedUriString(url, UriKind.Absolute))
             {
                 Console.WriteLine("Specified URL is malformed.");
@@ -27,40 +37,64 @@ namespace dev.obx.InventoryCollector
 
             SystemInfo si = new SystemInfo();
 
+            // Did they specify an asset tag?
+            if(args.Length == 2)
+            {
+                si.Asset_tag = args[1];
+            }
+
+            // Try to collect the data. Exit if we error.
             try
             {
                 si.CollectInfo();
             } catch(Exception e)
             {
                 Console.WriteLine("Exception Occurred:\n" + e.ToString());
+                Environment.Exit(99);
             }
 
+            // Dump the data to console.
             si.Dump();
 
+            // Post the data to the server.
             si.PostData(url);
 
         }
     }
 
+
+    /// <summary>
+    /// Class to collect system information and post to dataloader on inventory server. Asset Tag can be specified so that dataloader will use that instead of the system UUID for the asset_tag field in the database.
+    /// </summary>
     class SystemInfo
     {
-        String hostname;
-        String serial;
-        String uuid;
-        String os;
-        String os_version;
-        String service_pack;
-        String build_number;
-        String architecture;
-        String installed_memory;
-        String mac_address;
-        String install_date;
-        String manufacturer;
-        String model_sku;
-        String model_name;
+        private String hostname;
+        private String serial;
+        private String uuid;
+        private String os;
+        private String os_version;
+        private String service_pack;
+        private String build_number;
+        private String architecture;
+        private String installed_memory;
+        private String mac_address;
+        private String install_date;
+        private String manufacturer;
+        private String model_sku;
+        private String model_name;
+        private String ip_address;
+        private String public_ip;
+        private String asset_tag = null;
 
+        public string Asset_tag { get => asset_tag; set => asset_tag = value; }
+
+        /// <summary>
+        /// Post the data to the specified URL. If unsuccessful, sleep 5 minutes and try again (do/until).
+        /// </summary>
+        /// <param name="url">Absolute and well formed URL of dataloader.</param>
         public void PostData(string url)
         {
+            // Base values to post.
             var values = new System.Collections.Specialized.NameValueCollection()
             {
                 {"hostname",            hostname },
@@ -73,6 +107,8 @@ namespace dev.obx.InventoryCollector
                 {"architecture",        architecture },
                 {"installed_memory",    installed_memory },
                 {"mac_address",         mac_address },
+                {"ip_address",          ip_address },
+                {"public_ip",           public_ip },
                 {"install_date",        install_date },
                 {"manufacturer",        manufacturer },
                 {"model_sku",           model_sku },
@@ -80,33 +116,63 @@ namespace dev.obx.InventoryCollector
 
             };
 
+            // Add asset tag if it was specified.
+            if(asset_tag != null)
+            {
+                values.Add("asset_tag", asset_tag);
+            }
+
             using (WebClient client = new WebClient())
             {
-                byte[] response = client.UploadValues(url, values);
-                string result = System.Text.Encoding.UTF8.GetString(response);
+                Boolean done = false;
+                string result = "";
+
+                // Keep trying to post data every 5 minutes until it successfully posts.
+                do
+                {
+                    try
+                    {
+                        byte[] response = client.UploadValues(url, values);
+                        result = System.Text.Encoding.UTF8.GetString(response);
+                        done = true;
+                    } catch(WebException e)
+                    {
+                        Console.WriteLine("WebException: " + e.Message);
+                        Thread.Sleep(300000);
+                    }
+                } while (!done);
 
                 Console.WriteLine(result);
             }
         }
 
+        /// <summary>
+        /// Dumps the collected data for debug purposes.
+        /// </summary>
         public void Dump()
         {
-            Console.WriteLine("Hostname:         " + hostname);
-            Console.WriteLine("Serial Number:    " + serial);
-            Console.WriteLine("UUID:             " + uuid);
-            Console.WriteLine("OS:               " + os);
-            Console.WriteLine("OS Version:       " + os_version);
-            Console.WriteLine("Service Pack:     " + service_pack);
-            Console.WriteLine("Build Number:     " + build_number);
-            Console.WriteLine("Architecture:     " + architecture);
-            Console.WriteLine("Installed Memory: " + installed_memory);
-            Console.WriteLine("MAC Address:      " + mac_address);
-            Console.WriteLine("Install Date:     " + install_date);
-            Console.WriteLine("Manufacturer:     " + manufacturer);
-            Console.WriteLine("Model SKU:        " + model_sku);
-            Console.WriteLine("Model Name:       " + model_name);
+            Console.WriteLine("Asset Tag:         " + asset_tag);
+            Console.WriteLine("Hostname:          " + hostname);
+            Console.WriteLine("Serial Number:     " + serial);
+            Console.WriteLine("UUID:              " + uuid);
+            Console.WriteLine("OS:                " + os);
+            Console.WriteLine("OS Version:        " + os_version);
+            Console.WriteLine("Service Pack:      " + service_pack);
+            Console.WriteLine("Build Number:      " + build_number);
+            Console.WriteLine("Architecture:      " + architecture);
+            Console.WriteLine("Installed Memory:  " + installed_memory);
+            Console.WriteLine("MAC Address:       " + mac_address);
+            Console.WriteLine("Ip Address         " + ip_address);
+            Console.WriteLine("Public IP Address: " + public_ip);
+            Console.WriteLine("Install Date:      " + install_date);
+            Console.WriteLine("Manufacturer:      " + manufacturer);
+            Console.WriteLine("Model SKU:         " + model_sku);
+            Console.WriteLine("Model Name:        " + model_name);
         }
 
+        /// <summary>
+        /// Collect the required system info. TODO: Could use some cleanup/streamlining.
+        /// </summary>
         public void CollectInfo()
         {
             ManagementObjectSearcher s = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystemProduct");
@@ -164,6 +230,7 @@ namespace dev.obx.InventoryCollector
                             } catch(Exception e)
                             {
                                 Console.WriteLine("Exception parsing: " + property.Value);
+                                Console.WriteLine(e.Message);
                             }
                             break;
                         case "ServicePackMajorVersion":
@@ -181,6 +248,7 @@ namespace dev.obx.InventoryCollector
                 }
             }
 
+            // Get the MAC address of the first NIC.
             var macAddress =
                 (
                     from nic in NetworkInterface.GetAllNetworkInterfaces()
@@ -188,6 +256,42 @@ namespace dev.obx.InventoryCollector
                     select nic.GetPhysicalAddress().ToString()
                 ).FirstOrDefault();
             this.mac_address = macAddress;
+
+            // Get the IPv4 address of this computer.
+            IPInterfaceProperties ipProperties =
+                (
+                    from nic in NetworkInterface.GetAllNetworkInterfaces()
+                    where nic.OperationalStatus == OperationalStatus.Up && nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet && !nic.Description.Contains("Loopback") && !nic.Description.Contains("Virtual") && nic.Supports(NetworkInterfaceComponent.IPv4) == true
+                    select nic.GetIPProperties()
+                ).FirstOrDefault();
+            
+            foreach (UnicastIPAddressInformation addr in ipProperties.UnicastAddresses)
+            {
+                if(addr.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                this.ip_address = addr.Address.ToString();
+            }
+
+            // Get the traceroute to Google DNS and then get the first public subnet IPv4 address.
+            Traceroute traceroute = new Traceroute("8.8.8.8");
+            List<IPAddress> trace = traceroute.GetTrace();
+            foreach (IPAddress addr in trace)
+            {
+                if (addr.ToString().Equals("0.0.0.0")) continue;
+
+                int[] parts = addr.ToString().Split('.').Select(st => int.Parse(st)).ToArray();
+
+                if(  parts[0] == 10 ||
+                    (parts[0] == 192 && parts[1] == 168) ||
+                    (parts[0] == 172 && (parts[1] >= 16 && parts[1] <= 31)))
+                {
+                    // private subnet, try next
+                    continue;
+                }
+
+                // Found the first public subnet address, set it and break out of loop 
+                public_ip = addr.ToString();
+                break;
+            }
         }
     }
 }
---
+++ ConsoleApp1/Traceroute.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace dev.obx.InventoryCollector
+{
+    /// <summary>
+    /// This class creates an IPv4 traceroute of just host addresses (no timings) between the caller and the destination address.
+    /// Any host that does not reply within the timeout of 500ms is added as 0.0.0.0.
+    /// </summary>
+    public class Traceroute
+    {
+        private String destination = null;
+        private List<IPAddress> trace = new List<IPAddress>();
+        private IPStatus status;
+
+        public List<IPAddress> Trace { get => trace; set => trace = value; }
+
+        public string Destination { get => destination; set => destination = value; }
+
+        public IPStatus Status => status;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="Destination">Destination IPv4 address or DNS host.</param>
+        public Traceroute(String Destination)
+        {
+            this.Destination = Destination;
+        }
+
+        /// <summary>
+        /// Execute and return the trace to the destination.
+        /// </summary>
+        /// <returns>The list of IPv4 address between caller and destination.</returns>
+        public List<IPAddress> GetTrace()
+        {
+            // Start at TTL = 1 and do not fragment.
+            PingOptions pingOpts = new PingOptions(1, true);
+            Boolean done = false;
+            Ping ping = new Ping();
+            PingReply reply;
+            // Create a 32 byte payload
+            byte[] buffer = Encoding.ASCII.GetBytes("12345678901234567890123456789012");
+            byte[] nulladdr = new byte[] { 0, 0, 0, 0 };
+
+            // Stop after done or 64 hops.
+            while ( done == false && pingOpts.Ttl <=64 )
+            {
+                reply = ping.Send(destination, 1000, buffer, pingOpts);
+
+                // Track the last reply status.
+                this.status = reply.Status;
+
+                // If reply address is null add 0.0.0.0, else the IPv4 address of the ICMP reply.
+                if (reply.Address == null)
+                {
+                    trace.Add(new IPAddress(nulladdr));
+                }
+                else
+                {
+                    trace.Add(reply.Address);
+                }
+                
+                // Catch all statuses for which we need to increment TTL and go again. Default to being done for all other statuses.
+                switch (this.status) {
+                    case IPStatus.TimedOut:
+                    case IPStatus.TtlExpired:
+                        pingOpts.Ttl++;
+                        break;
+                    default:
+                        done = true;
+                        break;
+                }
+            }
+
+            return trace;
+        }
+    }
+}
